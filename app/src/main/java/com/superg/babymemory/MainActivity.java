b81447a1469ffce9f5a861bcec595619b3a6a18f
ImSuperG <18651564785@163.com>
2015-09-25 14:52:18 +0800

    add color.xml

--- app/src/main/java/com/superg/babymemory/MainActivity.java
+++ app/src/main/java/com/superg/babymemory/MainActivity.java
@@ -1,23 +1,55 @@
 package com.superg.babymemory;
 
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.superg.babymemory.fragments.EventFragment;
+import com.superg.babymemory.fragments.ImageFragment;
+import com.superg.babymemory.fragments.NearbyFragment;
+import com.superg.babymemory.fragments.SettingFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentTabHost mTabHost;
+    private LayoutInflater mInflater;
+    private int[] mImageSources = new int[]{R.drawable.event_bg_select, R.drawable.image_bg_select,
+            R.drawable.nearby_bg_select, R.drawable.setting_bg_select};
+    private String[] mTextArray = new String[]{"Event", "Pictures", "NearBy", "Setting"};
+    private Class[] mFragmentArray = new Class[]{EventFragment.class, ImageFragment.class, NearbyFragment.class, SettingFragment.class};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initTabHost();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void initTabHost() {
+        mInflater = LayoutInflater.from(this);
+        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+
+        mTabHost.addTab(mTabHost.newTabSpec("Event").setIndicator(getTabItemView(0)), EventFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("Pictures").setIndicator(getTabItemView(1)), ImageFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("Nearby").setIndicator(getTabItemView(2)), NearbyFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("Setting").setIndicator(getTabItemView(3)), SettingFragment.class, null);
+
+    }
+
+    private View getTabItemView(int index) {
+        View view = mInflater.inflate(R.layout.tab_item_view, null);
+        ImageView iv = (ImageView) view.findViewById(R.id.iv);
+        iv.setImageResource(mImageSources[index]);
+        TextView tv = (TextView) view.findViewById(R.id.tv);
+        tv.setText(mTextArray[index]);
+        return view;
     }
 
     @Override
---
+++ app/src/main/java/com/superg/babymemory/fragments/SettingFragment.java
@@ -0,0 +1,29 @@
+package com.superg.babymemory.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by rxy on 2015-09-24.
+ */
+public class SettingFragment extends Fragment {
+
+    private static final String TAG="SettingFragment";
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        if(isVisibleToUser){
+            Log.d(TAG,"It is already visible to user,you can load now.");
+        }
+        super.setUserVisibleHint(isVisibleToUser);
+    }
+}
